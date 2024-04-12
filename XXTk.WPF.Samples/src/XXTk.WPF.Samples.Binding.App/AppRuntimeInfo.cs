599130791764852b223eb2f9ff4e2d9a70c94185
xiaoxiao <jiajianj2057@qq.com>
2024-04-12 18:35:28 +0800

    feat: add binding sample

---
+++ XXTk.WPF.Samples/src/XXTk.WPF.Samples.Binding.App/AppRuntimeInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace XXTk.WPF.Samples.BindingSample.App
+{
+    public static class AppRuntimeInfo
+    {
+        public static string UserName { get; set; } = "xiaoxiaotank";
+
+        public static AppVersion AppVersion { get; set; } = new AppVersion { Version = "1.0.0.0" };
+
+        public static class MyBrushes
+        {
+            public static Brush Red { get; set; } = Brushes.Red;
+        }
+
+        public enum Gender
+        {
+            Female,
+            Male
+        }
+    }
+    
+
+    public class AppVersion
+    {
+        public string Version { get; set; }
+    }
+}
---
+++ XXTk.WPF.Samples/src/XXTk.WPF.Samples.Binding.App/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XXTk.WPF.Samples.BindingSample.App;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _name = "xiaoxiaotank";
+}
