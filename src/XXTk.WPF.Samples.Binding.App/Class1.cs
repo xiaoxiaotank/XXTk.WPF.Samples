824c303ea094d842e66fe7f6fd827edfe5f84900
xiaoxiao <jiajianj2057@qq.com>
2025-04-09 17:19:07 +0800

    1

---
+++ src/XXTk.WPF.Samples.Binding.App/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XXTk.WPF.Samples.BindingSample.App
+{
+    class Class1
+    {
+        // test1test1test1test1test1test1test1test1test1test1test1test1test1
+    }
+}
